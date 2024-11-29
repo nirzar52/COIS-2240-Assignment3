e161c40f1e57f3c66fee55a3398c4a6a3cf20e24
Nirzar Patel <nirzarpatel@trentu.ca>
2024-11-28 19:51:40 -0500

    [Transaction Class] Private instance + Singleton structure
    Added Singleton structure to Transaction class and implemented private instance in LibraryManagement class

--- Transaction.java
+++ Transaction.java
@@ -2,9 +2,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
+	
+	//	Private static instance
+	private static Transaction instance;
+	
+	//	Private constructor
+	private Transaction() {}
+	
+	// Public static accessor method
+    public static Transaction geTransaction() {
+    	if (instance == null) {
+			instance = new Transaction();
+		}
+    	return instance;
+    }
 
     // Perform the borrowing of a book
-    public static boolean borrowBook(Book book, Member member) {
+    public boolean borrowBook(Book book, Member member) {
         if (book.isAvailable()) {
             book.borrowBook();
             member.borrowBook(book); 
@@ -18,7 +32,7 @@ public class Transaction {
     }
 
     // Perform the returning of a book
-    public static void returnBook(Book book, Member member) {
+    public void returnBook(Book book, Member member) {
         if (member.getBorrowedBooks().contains(book)) {
             member.returnBook(book);
             book.returnBook();
@@ -30,7 +44,7 @@ public class Transaction {
     }
 
     // Get the current date and time in a readable format
-    private static String getCurrentDateTime() {
+    private String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
     }
